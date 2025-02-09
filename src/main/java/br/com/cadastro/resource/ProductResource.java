8b845b3852b8ee74dca962dabf73383d84bb82a3
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-02-08 22:44:03 -0300

    Added post, put and delete methods to the productResource

--- src/main/java/br/com/cadastro/resource/ProductResource.java
+++ src/main/java/br/com/cadastro/resource/ProductResource.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +27,7 @@ public class ProductResource {
 	
 	// Get methods
 	
-	// Get
+	// Get Method
 	// by id
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Product> findById(@PathVariable Integer id) {
@@ -31,12 +35,39 @@ public class ProductResource {
 		return ResponseEntity.ok().body(product);
 	}
 	
-	// GetAll
+	// GetAll Method
 	// all
 	@GetMapping
 	public ResponseEntity<List<Product>> findAll() {
 		List<Product> products = service.findAll();
 		return ResponseEntity.ok().body(products);
+	}
+	
+	// Post Method
+	
+	// adds a product
+	@PostMapping
+	public ResponseEntity<Product> save(@RequestBody Product product) {
+		Product productSaved = service.save(product);
+		return ResponseEntity.ok().body(productSaved);
+	}
+	
+	// Put Method
+	
+	// Updated a product
+	@PutMapping
+	public ResponseEntity<Product> update(@RequestBody Product product) {
+		Product productUpdated = service.update(product);
+		return ResponseEntity.ok().body(productUpdated);
+	}
+	
+	// Delete Method
+	
+	// Deletes a product
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
